08bd37b8d54b4b6f9855b4aa9cd34185dbd62652
Martijn <fmvbeest@gmail.com>
2021-12-09 20:24:11 +0100

    AoC 2021 Project

---
+++ Puzzles/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Puzzles
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System.Diagnostics;
+using Xunit;
+
+namespace Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        Assert.False(false);
+    }
+}
